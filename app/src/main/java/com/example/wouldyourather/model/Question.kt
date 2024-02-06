7f4f0272812ac56f59bb936cf422b7d004dde330
Bibek Bhujel <biekbhujel077@gmail.com>
2024-02-06 20:22:26 +0545

    Adding some documentations

--- app/src/main/java/com/example/wouldyourather/model/Question.kt
+++ app/src/main/java/com/example/wouldyourather/model/Question.kt
@@ -1,6 +1,7 @@
 package com.example.wouldyourather.model
 
 
+// this is a question class that holds the questions to be in the card
 data class Question(
     val question: String,
     val options: Pair<Option, Option>
